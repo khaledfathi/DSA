bb3ba949e7f690a8addb9e8c6885acc0b18c43bd
Khaled Fathi <dev@khaledfathi.com>
2025-02-27 19:04:17 +0200

    inline docs for stack

--- inc/stack_list.hpp
+++ inc/stack_list.hpp
@@ -2,7 +2,7 @@
  * File : stack_list.hpp
  * Namespace : 
  * Created : Wed Feb 19 2025
- * Modified : Sun Feb 23 2025
+ * Modified : Thu Feb 27 2025
  * Author : Khaled Fathi
  * Email : dev@khaledfathi.com
  * 
@@ -19,12 +19,43 @@ namespace dsa {
     template <typename T>
     class StackList {
         private : 
+            /**
+             * @brief Linked list of type [T] , used for storing stack elements
+             * 
+             */
             List<T> arr = List<T>(); 
         public : 
+            /**
+             * @brief push new element to the stack  
+             * 
+             * @param value element to be added to stack 
+             */
             void push (T value); 
+            /**
+             * @brief pop out element from stack
+             * 
+             * @return T return element has been popped out
+             */
             T pop (); 
+            /**
+             * @brief check if stack is empty 
+             * 
+             * @return true = stack is empty 
+             * @return false = stack is not empty 
+             */
             bool isEmpty(); 
+            /**
+             * @brief get the top element in the stack , put not popped out from the stack
+             * 
+             * @return T  top element of the stack 
+             */
             T stackTop(); 
+            /**
+             * @brief get the peek value in the stack depend on the given position
+             * 
+             * @param position max position of stack 
+             * @return T value at the position 
+             */
             T peek(int position); 
     }; 
 }
